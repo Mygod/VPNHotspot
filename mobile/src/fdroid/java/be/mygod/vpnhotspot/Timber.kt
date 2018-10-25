d7825014213591452a9411ca25ba30cc0ad886b0
Mygod <contact-github@mygod.be>
2018-10-25 13:31:47 +0800

    Fix Timber tagging for fdroid builds

--- mobile/src/fdroid/java/be/mygod/vpnhotspot/Timber.kt
+++ mobile/src/fdroid/java/be/mygod/vpnhotspot/Timber.kt
@@ -4,4 +4,4 @@ import timber.log.Timber
 
 fun initTimber() = Timber.plant(Timber.DebugTree())
 
-fun debugLog(tag: String?, message: String?) = Timber.d(tag, message)
+fun debugLog(tag: String?, message: String?) = Timber.tag(tag).d(message)
