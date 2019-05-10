cb589560f7698456914e625f3883a7019cb984cf
Mygod <contact-git@mygod.be>
2019-05-10 11:27:34 +0800

    Disable tethering QS tiles if receiver does not work
    Also fixes a memory leak.

--- mobile/src/main/java/be/mygod/vpnhotspot/manage/TetherListeningTileService.kt
+++ mobile/src/main/java/be/mygod/vpnhotspot/manage/TetherListeningTileService.kt
@@ -9,7 +9,7 @@ import be.mygod.vpnhotspot.util.broadcastReceiver
 
 @RequiresApi(24)
 abstract class TetherListeningTileService : KillableTileService() {
-    protected var tethered: List<String> = emptyList()
+    protected var tethered: List<String>? = null
 
     private val receiver = broadcastReceiver { _, intent ->
         tethered = intent.tetheredIfaces
@@ -18,8 +18,9 @@ abstract class TetherListeningTileService : KillableTileService() {
 
     override fun onStartListening() {
         super.onStartListening()
-        val intent = registerReceiver(receiver, IntentFilter(TetheringManager.ACTION_TETHER_STATE_CHANGED))
-        if (intent != null) tethered = intent.tetheredIfaces
+        tethered = registerReceiver(receiver, IntentFilter(TetheringManager.ACTION_TETHER_STATE_CHANGED))
+                ?.tetheredIfaces
+        updateTile()
     }
 
     override fun onStopListening() {
