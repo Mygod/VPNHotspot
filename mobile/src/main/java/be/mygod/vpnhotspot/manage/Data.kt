56a2c8bb4bf3f883ea27c9792cf22ff4a418bc99
Mygod <contact-git@mygod.be>
2019-09-19 11:57:25 +0800

    Remove redundant enabled

--- mobile/src/main/java/be/mygod/vpnhotspot/manage/Data.kt
+++ mobile/src/main/java/be/mygod/vpnhotspot/manage/Data.kt
@@ -7,6 +7,5 @@ abstract class Data : BaseObservable() {
     abstract val title: CharSequence
     abstract val text: CharSequence
     abstract val active: Boolean
-    open val enabled get() = true
     open val selectable get() = true
 }
