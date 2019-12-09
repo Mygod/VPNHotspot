1709ffd8ffdfc5b31da6921550d6df0a0030571e
Mygod <contact-git@mygod.be>
2019-12-09 21:57:11 +0800

    Fix Wi-Fi QR code with non-ISO chars

--- mobile/src/main/java/be/mygod/vpnhotspot/util/QRCodeDialog.kt
+++ mobile/src/main/java/be/mygod/vpnhotspot/util/QRCodeDialog.kt
@@ -1,26 +1,53 @@
 package be.mygod.vpnhotspot.util
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
 import be.mygod.vpnhotspot.R
-import net.glxn.qrgen.android.QRCode
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
+import timber.log.Timber
+import java.nio.charset.StandardCharsets
 
+/**
+ * Based on:
+ * https://android.googlesource.com/platform/packages/apps/Settings/+/0d706f0/src/com/android/settings/wifi/qrcode/QrCodeGenerator.java
+ * https://android.googlesource.com/platform/packages/apps/Settings/+/8a9ccfd/src/com/android/settings/wifi/dpp/WifiDppQrCodeGeneratorFragment.java#153
+ */
 class QRCodeDialog : DialogFragment() {
     companion object {
         private const val KEY_ARG = "arg"
+        private val iso88591 = StandardCharsets.ISO_8859_1.newEncoder()
     }
 
     fun withArg(arg: String) = apply { arguments = bundleOf(KEY_ARG to arg) }
     private val arg get() = arguments?.getString(KEY_ARG)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
-            ImageView(context).apply {
-                val size = resources.getDimensionPixelSize(R.dimen.qr_code_size)
-                layoutParams = ViewGroup.LayoutParams(size, size)
-                setImageBitmap((QRCode.from(arg).withSize(size, size) as QRCode).bitmap())
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) = try {
+        ImageView(context).apply {
+            val hints = HashMap<EncodeHintType, Any>()
+            if (!iso88591.canEncode(arg)) hints[EncodeHintType.CHARACTER_SET] = StandardCharsets.UTF_8.name()
+            val size = resources.getDimensionPixelSize(R.dimen.qrcode_size)
+            val qrBits = MultiFormatWriter().encode(arg, BarcodeFormat.QR_CODE, size, size, hints)
+            val bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.RGB_565)
+            for (x in 0 until size) for (y in 0 until size) {
+                bitmap.setPixel(x, y, if (qrBits.get(x, y)) Color.BLACK else Color.WHITE)
             }
+            layoutParams = ViewGroup.LayoutParams(size, size)
+            setImageBitmap(bitmap)
+        }
+    } catch (e: WriterException) {
+        Timber.w(e)
+        Toast.makeText(context, e.readableMessage, Toast.LENGTH_LONG).show()
+        dismiss()
+        null
+    }
 }
