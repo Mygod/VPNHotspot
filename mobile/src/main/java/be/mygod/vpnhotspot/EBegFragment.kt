985f75c6221ff5b415fe12b1b468ab86ec95c206
Mygod <contact-git@mygod.be>
2019-12-13 21:28:01 +0800

    Fix various coroutines related issues

--- mobile/src/main/java/be/mygod/vpnhotspot/EBegFragment.kt
+++ mobile/src/main/java/be/mygod/vpnhotspot/EBegFragment.kt
@@ -8,18 +8,22 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.lifecycle.lifecycleScope
 import be.mygod.vpnhotspot.App.Companion.app
 import be.mygod.vpnhotspot.util.launchUrl
 import be.mygod.vpnhotspot.widget.SmartSnackbar
 import com.android.billingclient.api.*
 import kotlinx.android.synthetic.main.fragment_ebeg.view.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
 /**
  * Based on: https://github.com/PrivacyApps/donations/blob/747d36a18433c7e9329691054122a8ad337a62d2/Donations/src/main/java/org/sufficientlysecure/donations/DonationsFragment.java
  */
-class EBegFragment : AppCompatDialogFragment(), SkuDetailsResponseListener {
-    companion object : BillingClientStateListener, PurchasesUpdatedListener, ConsumeResponseListener {
+class EBegFragment : AppCompatDialogFragment() {
+    companion object : BillingClientStateListener, PurchasesUpdatedListener {
         private lateinit var billingClient: BillingClient
 
         fun init() {
@@ -46,23 +50,23 @@ class EBegFragment : AppCompatDialogFragment(), SkuDetailsResponseListener {
         override fun onPurchasesUpdated(billingResult: BillingResult?, purchases: MutableList<Purchase>?) {
             if (billingResult?.responseCode == BillingClient.BillingResponseCode.OK && purchases != null) {
                 // directly consume in-app purchase, so that people can donate multiple times
-                for (purchase in purchases) if (purchase.purchaseState == Purchase.PurchaseState.PURCHASED) {
-                    billingClient.consumeAsync(ConsumeParams.newBuilder().apply {
-                        setPurchaseToken(purchase.purchaseToken)
-                    }.build(), this)
-                }
+                purchases.filter { it.purchaseState == Purchase.PurchaseState.PURCHASED }.map(this::consumePurchase)
             } else Timber.e("onPurchasesUpdated: ${billingResult?.responseCode}")
         }
 
-        override fun onConsumeResponse(billingResult: BillingResult?, purchaseToken: String?) {
-            if (billingResult?.responseCode == BillingClient.BillingResponseCode.OK) {
-                SmartSnackbar.make(R.string.donations__thanks_dialog).show()
-            } else Timber.e("onConsumeResponse: ${billingResult?.responseCode}")
+        private fun consumePurchase(purchase: Purchase) = GlobalScope.launch(Dispatchers.Main.immediate) {
+            billingClient.consumePurchase(ConsumeParams.newBuilder().apply {
+                setPurchaseToken(purchase.purchaseToken)
+            }.build()).apply {
+                if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+                    SmartSnackbar.make(R.string.donations__thanks_dialog).show()
+                } else Timber.e("onConsumeResponse: ${billingResult.responseCode}")
+            }
         }
     }
 
     private lateinit var googleSpinner: Spinner
-    private var skus: MutableList<SkuDetails>? = null
+    private var skus: List<SkuDetails>? = null
         set(value) {
             field = value
             googleSpinner.apply {
@@ -79,12 +83,18 @@ class EBegFragment : AppCompatDialogFragment(), SkuDetailsResponseListener {
         super.onViewCreated(view, savedInstanceState)
         dialog!!.setTitle(R.string.settings_misc_donate)
         googleSpinner = view.donations__google_android_market_spinner
-        billingClient.querySkuDetailsAsync(
-                SkuDetailsParams.newBuilder().apply {
-                    setSkusList(listOf("donate001", "donate002", "donate005", "donate010", "donate020", "donate050",
-                            "donate100", "donate200", "donatemax"))
-                    setType(BillingClient.SkuType.INAPP)
-                }.build(), this)
+        lifecycleScope.launchWhenCreated {
+            billingClient.querySkuDetails(SkuDetailsParams.newBuilder().apply {
+                setSkusList(listOf("donate001", "donate002", "donate005", "donate010", "donate020", "donate050",
+                        "donate100", "donate200", "donatemax"))
+                setType(BillingClient.SkuType.INAPP)
+            }.build()).apply {
+                if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) skus = skuDetailsList else {
+                    Timber.e("onSkuDetailsResponse: ${billingResult.responseCode}")
+                    SmartSnackbar.make(R.string.donations__google_android_market_not_supported).show()
+                }
+            }
+        }
         view.donations__google_android_market_donate_button.setOnClickListener {
             val sku = skus?.getOrNull(googleSpinner.selectedItemPosition)
             if (sku != null) billingClient.launchBillingFlow(requireActivity(), BillingFlowParams.newBuilder().apply {
@@ -94,12 +104,5 @@ class EBegFragment : AppCompatDialogFragment(), SkuDetailsResponseListener {
         @Suppress("ConstantConditionIf")
         if (BuildConfig.DONATIONS) (view.donations__more_stub.inflate() as Button)
                 .setOnClickListener { requireContext().launchUrl("https://mygod.be/donate/") }
-    }
-
-    override fun onSkuDetailsResponse(billingResult: BillingResult?, skuDetailsList: MutableList<SkuDetails>?) {
-        if (billingResult?.responseCode == BillingClient.BillingResponseCode.OK) skus = skuDetailsList else {
-            Timber.e("onSkuDetailsResponse: ${billingResult?.responseCode}")
-            SmartSnackbar.make(R.string.donations__google_android_market_not_supported).show()
-        }
     }
 }
