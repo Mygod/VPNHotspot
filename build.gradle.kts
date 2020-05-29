677b2d8f8e8847390e7f42a3191fd5f6281b6fac
Mygod <contact-git@mygod.be>
2020-05-29 01:12:00 -0400

    Refine build gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,12 +12,12 @@ buildscript {
     }
 
     dependencies {
+        classpath(kotlin("gradle-plugin", kotlinVersion))
         classpath("com.android.tools.build:gradle:4.0.0")
         classpath("com.github.ben-manes:gradle-versions-plugin:0.28.0")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.1.1")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.2")
         classpath("com.google.gms:google-services:4.3.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
 
