0bb756daebb81be4252d243d1eab6277ba8edd03
Mygod <contact-git@mygod.be>
2020-07-31 04:18:40 +0800

    Be more descriptive of unrecognized key mgmt

--- mobile/src/main/java/be/mygod/vpnhotspot/net/wifi/SoftApConfigurationCompat.kt
+++ mobile/src/main/java/be/mygod/vpnhotspot/net/wifi/SoftApConfigurationCompat.kt
@@ -219,7 +219,9 @@ data class SoftApConfigurationCompat(
                             SoftApConfiguration.SECURITY_TYPE_WPA2_PSK
                         }
                         android.net.wifi.WifiConfiguration.KeyMgmt.SAE -> SoftApConfiguration.SECURITY_TYPE_WPA3_SAE
-                        else -> throw IllegalArgumentException("Unrecognized key management: $allowedKeyManagement")
+                        else -> android.net.wifi.WifiConfiguration.KeyMgmt.strings.getOrElse(selected) { "?" }.let {
+                            throw IllegalArgumentException("Unrecognized key management $it ($selected)")
+                        }
                     }
                 },
                 if (Build.VERSION.SDK_INT >= 28) TetherTimeoutMonitor.enabled else false,
