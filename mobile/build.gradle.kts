9599378c3953cc27ce6a9cf1177e078e51316b55
Mygod <contact-git@mygod.be>
2020-08-08 04:06:58 +0800

    v2.10.13

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         minSdkVersion(21)
         targetSdkVersion(targetSdk)
         resConfigs(listOf("it", "ru", "zh-rCN", "zh-rTW"))
-        versionCode = 247
-        versionName = "2.10.12"
+        versionCode = 248
+        versionName = "2.10.13"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         javaCompileOptions.annotationProcessorOptions.arguments(mapOf(
                 "room.incremental" to "true",
