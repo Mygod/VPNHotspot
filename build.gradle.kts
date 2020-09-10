db62cb6be0df392fec034706e16b2177a0da05e4
Mygod <contact-git@mygod.be>
2020-09-11 06:48:30 +0800

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.github.ben-manes.versions") version "0.29.0"
+    id("com.github.ben-manes.versions") version "0.31.0"
 }
 
 buildscript {
@@ -9,9 +9,9 @@ buildscript {
     }
 
     dependencies {
-        classpath(kotlin("gradle-plugin", "1.4.0"))
-        classpath("com.android.tools.build:gradle:4.1.0-rc01")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.2.0")
+        classpath(kotlin("gradle-plugin", "1.4.10"))
+        classpath("com.android.tools.build:gradle:4.1.0-rc02")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.2.1")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.2")
         classpath("com.google.gms:google-services:4.3.3")
     }
