7905783c3dd10b434e4454524062df141e6842f8
Mygod <contact-git@mygod.be>
2021-04-16 16:13:00 -0400

    v2.11.4

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         minSdkVersion(21)
         targetSdkVersion(targetSdk)
         resConfigs(listOf("it", "ru", "zh-rCN", "zh-rTW"))
-        versionCode = 256
-        versionName = "2.11.3"
+        versionCode = 257
+        versionName = "2.11.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         javaCompileOptions.annotationProcessorOptions.arguments(mapOf(
                 "room.incremental" to "true",
@@ -91,10 +91,10 @@ dependencies {
     implementation("com.google.firebase:firebase-crashlytics:17.4.1")
     implementation("com.google.zxing:core:3.4.1")
     implementation("com.jakewharton.timber:timber:4.7.1")
-    implementation("com.linkedin.dexmaker:dexmaker:2.28.0")
+    implementation("com.linkedin.dexmaker:dexmaker:2.28.1")
     implementation("com.takisoft.preferencex:preferencex-simplemenu:1.1.0")
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.room:room-testing:$roomVersion")
     androidTestImplementation("androidx.test:runner:1.3.0")
