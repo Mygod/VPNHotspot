6cc251fb89ebaa393363d1a4acae47ed00285a13
Mygod <contact-git@mygod.be>
2021-10-29 21:44:15 -0400

    Update dependencies

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -66,15 +66,15 @@ android {
 }
 
 dependencies {
-    val lifecycleVersion = "2.4.0-rc01"
+    val lifecycleVersion = "2.4.0"
     val roomVersion = "2.4.0-beta01"
 
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
     kapt("androidx.room:room-compiler:$roomVersion")
     implementation(kotlin("stdlib-jdk8"))
-    implementation("androidx.appcompat:appcompat:1.3.1")    // https://issuetracker.google.com/issues/151603528
+    implementation("androidx.appcompat:appcompat:1.4.0-rc01")   // https://issuetracker.google.com/issues/151603528
     implementation("androidx.browser:browser:1.4.0-rc01")
-    implementation("androidx.core:core-ktx:1.7.0-rc01")
+    implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.emoji:emoji:1.1.0")
     implementation("androidx.fragment:fragment-ktx:1.3.6")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
@@ -84,7 +84,7 @@ dependencies {
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("com.android.billingclient:billing-ktx:4.0.0")
     implementation("com.google.android.gms:play-services-oss-licenses:17.0.0")
-    implementation("com.google.android.material:material:1.5.0-alpha04")
+    implementation("com.google.android.material:material:1.5.0-alpha05")
     implementation("com.google.firebase:firebase-analytics-ktx:19.0.2")
     implementation("com.google.firebase:firebase-crashlytics:18.2.3")
     implementation("com.google.zxing:core:3.4.1")
