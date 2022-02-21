ba0c62a0cf7c418ee1dcabb37a702fce9012addd
Mygod <contact-git@mygod.be>
2022-02-21 12:13:01 -0500

    Target Android 12L

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.github.ben-manes.versions") version "0.39.0"
+    id("com.github.ben-manes.versions") version "0.42.0"
 }
 
 buildscript {
@@ -9,8 +9,8 @@ buildscript {
     }
 
     dependencies {
-        classpath(kotlin("gradle-plugin", "1.6.0"))
-        classpath("com.android.tools.build:gradle:7.1.0-beta04")
+        classpath(kotlin("gradle-plugin", "1.6.10"))
+        classpath("com.android.tools.build:gradle:7.1.1")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.1")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.4")
         classpath("com.google.gms:google-services:4.3.10")
--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -10,13 +10,13 @@ plugins {
 
 android {
     val javaVersion = JavaVersion.VERSION_11
-    buildToolsVersion = "31.0.0"
+    buildToolsVersion = "32.0.0"
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = javaVersion
         targetCompatibility = javaVersion
     }
-    compileSdk = 31
+    compileSdk = 32
     kotlinOptions.jvmTarget = javaVersion.toString()
     defaultConfig {
         applicationId = "be.mygod.vpnhotspot"
@@ -56,10 +56,10 @@ android {
         }
         create("google") {
             dimension = "freedom"
-            targetSdk = 31
+            targetSdk = 32
             versionNameSuffix = "-g"
             buildConfigField("boolean", "DONATIONS", "false")
-            buildConfigField("int", "TARGET_SDK", "31")
+            buildConfigField("int", "TARGET_SDK", "32")
         }
     }
     sourceSets.getByName("androidTest").assets.srcDir("$projectDir/schemas")
@@ -67,33 +67,33 @@ android {
 
 dependencies {
     val lifecycleVersion = "2.4.0"
-    val roomVersion = "2.4.0-rc01"
+    val roomVersion = "2.4.1"
 
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
     kapt("androidx.room:room-compiler:$roomVersion")
     implementation(kotlin("stdlib-jdk8"))
-    implementation("androidx.appcompat:appcompat:1.4.0")    // https://issuetracker.google.com/issues/151603528
+    implementation("androidx.appcompat:appcompat:1.4.1")    // https://issuetracker.google.com/issues/151603528
     implementation("androidx.browser:browser:1.4.0")
     implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.fragment:fragment-ktx:1.4.0")
+    implementation("androidx.fragment:fragment-ktx:1.4.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
-    implementation("androidx.preference:preference:1.2.0-beta01")
+    implementation("androidx.preference:preference:1.2.0")
     implementation("androidx.room:room-ktx:$roomVersion")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("com.android.billingclient:billing-ktx:4.0.0")
     implementation("com.google.android.gms:play-services-oss-licenses:17.0.0")
-    implementation("com.google.android.material:material:1.5.0-beta01")
-    implementation("com.google.firebase:firebase-analytics-ktx:20.0.0")
-    implementation("com.google.firebase:firebase-crashlytics:18.2.5")
+    implementation("com.google.android.material:material:1.5.0")
+    implementation("com.google.firebase:firebase-analytics-ktx:20.1.0")
+    implementation("com.google.firebase:firebase-crashlytics:18.2.8")
     implementation("com.google.zxing:core:3.4.1")
     implementation("com.jakewharton.timber:timber:5.0.1")
     implementation("com.linkedin.dexmaker:dexmaker:2.28.1")
     implementation("com.takisoft.preferencex:preferencex-simplemenu:1.1.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0-RC")
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
     add("googleImplementation", "com.github.tiann:FreeReflection:3.1.0")
-    add("googleImplementation", "com.google.android.play:core:1.10.2")
+    add("googleImplementation", "com.google.android.play:core:1.10.3")
     add("googleImplementation", "com.google.android.play:core-ktx:1.8.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.room:room-testing:$roomVersion")
