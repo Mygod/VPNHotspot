9804c2c3d49f70503496697ccf8990c40acc3b29
Mygod <contact-git@mygod.be>
2022-12-04 11:54:43 -0500

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.github.ben-manes.versions") version "0.42.0"
+    id("com.github.ben-manes.versions") version "0.44.0"
 }
 
 buildscript {
@@ -9,8 +9,8 @@ buildscript {
     }
 
     dependencies {
-        classpath(kotlin("gradle-plugin", "1.7.10"))
-        classpath("com.android.tools.build:gradle:7.4.0-beta01")
+        classpath(kotlin("gradle-plugin", "1.7.21"))
+        classpath("com.android.tools.build:gradle:7.4.0-rc01")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.5")
         classpath("com.google.gms:google-services:4.3.14")
--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -70,26 +70,26 @@ android {
 
 dependencies {
     val lifecycleVersion = "2.5.1"
-    val roomVersion = "2.5.0-alpha03"
+    val roomVersion = "2.5.0-beta02"
 
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.0")
     kapt("androidx.room:room-compiler:$roomVersion")
     implementation(kotlin("stdlib-jdk8"))
-    implementation("androidx.browser:browser:1.4.0")
+    implementation("androidx.browser:browser:1.5.0-alpha01")
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.fragment:fragment-ktx:1.5.3")
+    implementation("androidx.fragment:fragment-ktx:1.5.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
     implementation("androidx.preference:preference:1.2.0")
     implementation("androidx.room:room-ktx:$roomVersion")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("be.mygod.librootkotlinx:librootkotlinx:1.0.0")
-    implementation("com.android.billingclient:billing-ktx:5.0.0")
+    implementation("com.android.billingclient:billing-ktx:5.1.0")
     implementation("com.google.android.gms:play-services-oss-licenses:17.0.0")
-    implementation("com.google.android.material:material:1.7.0-rc01")
-    implementation("com.google.firebase:firebase-analytics-ktx:21.1.1")
-    implementation("com.google.firebase:firebase-crashlytics:18.2.13")
-    implementation("com.google.zxing:core:3.5.0")
+    implementation("com.google.android.material:material:1.7.0")
+    implementation("com.google.firebase:firebase-analytics-ktx:21.2.0")
+    implementation("com.google.firebase:firebase-crashlytics:18.3.2")
+    implementation("com.google.zxing:core:3.5.1")
     implementation("com.jakewharton.timber:timber:5.0.1")
     implementation("com.linkedin.dexmaker:dexmaker:2.28.3")
     implementation("com.takisoft.preferencex:preferencex-simplemenu:1.1.0")
@@ -100,7 +100,7 @@ dependencies {
     add("googleImplementation", "com.google.android.play:core-ktx:1.8.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.room:room-testing:$roomVersion")
-    androidTestImplementation("androidx.test:runner:1.4.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.test.ext:junit-ktx:1.1.3")
+    androidTestImplementation("androidx.test:runner:1.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+    androidTestImplementation("androidx.test.ext:junit-ktx:1.1.4")
 }
