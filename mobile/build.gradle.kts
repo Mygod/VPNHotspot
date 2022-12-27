4d6e0c944d8133b36a773068a8a3d727ce22f0d4
Mygod <contact-git@mygod.be>
2022-12-26 22:48:16 -0500

    v2.15.4

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         minSdk = 21
         targetSdk = 33
         resourceConfigurations.addAll(arrayOf("it", "pt-rBR", "ru", "zh-rCN", "zh-rTW"))
-        versionCode = 303
-        versionName = "2.15.3"
+        versionCode = 304
+        versionName = "2.15.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         javaCompileOptions.annotationProcessorOptions.arguments.apply {
             put("room.expandProjection", "true")
