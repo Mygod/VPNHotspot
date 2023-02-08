8ba727c1b8388ec515b64230f30e3e4025f0baa5
Mygod <contact-git@mygod.be>
2023-02-08 18:34:34 -0500

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,13 @@
 plugins {
-    id("com.github.ben-manes.versions") version "0.44.0"
+    id("com.android.application") version "7.4.1" apply false
+    id("com.github.ben-manes.versions") version "0.45.0"
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
 
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-
     dependencies {
-        classpath(kotlin("gradle-plugin", "1.8.0"))
-        classpath("com.android.tools.build:gradle:7.4.0-rc01")  // fixed to prevent crash on Android 8.1-
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
         classpath("com.google.gms:google-services:4.3.15")
     }
-}
-
-allprojects {
-    repositories {
-        google()
-        jcenter()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
 }
--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     namespace = "be.mygod.vpnhotspot"
 
     val javaVersion = JavaVersion.VERSION_11
-    buildToolsVersion = "33.0.1"
+    buildToolsVersion = "33.0.2"
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = javaVersion
@@ -25,8 +25,8 @@ android {
         minSdk = 21
         targetSdk = 33
         resourceConfigurations.addAll(arrayOf("it", "pt-rBR", "ru", "zh-rCN", "zh-rTW"))
-        versionCode = 305
-        versionName = "2.15.5"
+        versionCode = 1000
+        versionName = "2.16.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         javaCompileOptions.annotationProcessorOptions.arguments.apply {
             put("room.expandProjection", "true")
@@ -68,10 +68,10 @@ dependencies {
     val lifecycleVersion = "2.5.1"
     val roomVersion = "2.5.0"
 
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.0")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.2")
     kapt("androidx.room:room-compiler:$roomVersion")
     implementation(kotlin("stdlib-jdk8"))
-    implementation("androidx.browser:browser:1.5.0-beta01")
+    implementation("androidx.browser:browser:1.5.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.fragment:fragment-ktx:1.5.5")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
@@ -84,7 +84,7 @@ dependencies {
     implementation("com.github.tiann:FreeReflection:3.1.0")
     implementation("com.google.android.gms:play-services-base:18.1.0")  // fix for GoogleApiActivity crash
     implementation("com.google.android.gms:play-services-oss-licenses:17.0.0")
-    implementation("com.google.android.material:material:1.8.0-rc01")
+    implementation("com.google.android.material:material:1.8.0")
     implementation("com.google.firebase:firebase-analytics-ktx:21.2.0")
     implementation("com.google.firebase:firebase-crashlytics:18.3.3")
     implementation("com.google.zxing:core:3.5.1")
