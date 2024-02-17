f112b30600a19272d5867fd0d5fbff6f9e7e099e
Mygod <contact-git@mygod.be>
2024-02-16 21:25:49 -0500

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application") version "8.3.0-beta02" apply false
-    id("com.github.ben-manes.versions") version "0.50.0"
-    id("com.google.devtools.ksp") version "1.9.22-1.0.16" apply false
+    id("com.android.application") version "8.3.0-rc02" apply false
+    id("com.github.ben-manes.versions") version "0.51.0"
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
@@ -9,6 +9,6 @@ buildscript {
     dependencies {
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
-        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.gms:google-services:4.4.1")
     }
 }
