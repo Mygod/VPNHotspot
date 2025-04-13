f2a2a82b496f64e9d760d3f77f62eff22a758c9a
Mygod <contact-git@mygod.be>
2025-04-13 00:21:42 -0700

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.android.application") version "8.9.0" apply false
+    id("com.android.application") version "8.9.1" apply false
     id("com.github.ben-manes.versions") version "0.52.0"
-    id("com.google.devtools.ksp") version "2.1.10-1.0.31" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
+    id("com.google.devtools.ksp") version "2.1.20-2.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
 }
 
 buildscript {
