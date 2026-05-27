4e20b8e6548a933b46013d8cea5e5135629100ff
Mygod <contact-git@mygod.be>
2026-05-27 11:06:22 -0400

    Only warn if fallback fails

--- mobile/src/main/java/be/mygod/vpnhotspot/manage/ManageBar.kt
+++ mobile/src/main/java/be/mygod/vpnhotspot/manage/ManageBar.kt
@@ -2,20 +2,25 @@ package be.mygod.vpnhotspot.manage
 
 import android.content.Intent
 import android.os.Build
+import be.mygod.vpnhotspot.App.Companion.app
 import timber.log.Timber
 
 object ManageBar {
+    private const val ACTION_TETHER_SETTINGS = "android.settings.TETHER_SETTINGS"
     fun start(startActivity: (Intent) -> Unit) {
+        var eSuppressed: RuntimeException? = null
         if (Build.VERSION.SDK_INT >= 30) try {
-            startActivity(Intent("android.settings.TETHER_SETTINGS").setFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+            startActivity(Intent(ACTION_TETHER_SETTINGS).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
             return
         } catch (e: RuntimeException) {
-            Timber.w(e)
+            app.logEvent(ACTION_TETHER_SETTINGS)
+            eSuppressed = e
         }
         try {
             startActivity(Intent().setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 .setClassName("com.android.settings", "com.android.settings.Settings\$TetherSettingsActivity"))
         } catch (e: RuntimeException) {
+            eSuppressed?.let { e.addSuppressed(eSuppressed) }
             Timber.w(e)
         }
     }
