850d85cf95cae85b8888af863c56c86660b528aa
Mygod <contact-git@mygod.be>
2026-06-18 14:20:43 -0400

    Observe vendor API

---
+++ mobile/src/hiddenApiStubs/java/android/os/SystemProperties.java
@@ -0,0 +1,12 @@
+package android.os;
+
+public class SystemProperties {
+    /**
+     * Used during Crashlytics startup to record raw vendor partition API context.
+     *
+     * Source: https://android.googlesource.com/platform/frameworks/base/+/1cdfff555f4a21f71ccc978290e2e212e2f8b168/core/java/android/os/SystemProperties.java#154
+     */
+    public static String get(String key, String def) {
+        throw new UnsupportedOperationException();
+    }
+}
