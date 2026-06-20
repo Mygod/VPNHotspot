2997ced6a8f23215c7be265bf86b33d3b7764c08
Mygod <contact-git@mygod.be>
2026-06-19 22:56:30 -0400

    v3.0.6

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -230,8 +230,8 @@ android {
         applicationId = "be.mygod.vpnhotspot"
         minSdk = 29
         targetSdk = 37
-        versionCode = 2008
-        versionName = "3.0.5"
+        versionCode = 2009
+        versionName = "3.0.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     splits {
